7e6cd00ad003903f70fc56cb6b354e870372e510
kirtana4 <konduru148@gmail.com>
2021-09-02 17:47:23 +0530

    shopping

---
+++ E-Commerce-Template/src/components/Footer.jsx
@@ -0,0 +1,209 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as IconTelephone } from "bootstrap-icons/icons/telephone.svg";
+import { ReactComponent as IconEnvelope } from "bootstrap-icons/icons/envelope.svg";
+import { ReactComponent as IconBriefcase } from "bootstrap-icons/icons/briefcase.svg";
+import { ReactComponent as IconBadgeAd } from "bootstrap-icons/icons/badge-ad.svg";
+import { ReactComponent as IconGift } from "bootstrap-icons/icons/gift.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faFacebookF,
+  faInstagram,
+  faYoutube,
+  faApple,
+  faWindows,
+  faAndroid,
+} from "@fortawesome/free-brands-svg-icons";
+
+const Footer = () => {
+  return (
+    <React.Fragment>
+      <footer>
+        <div className="container-fluid bg-primary">
+          <div className="row ">
+            <div className="col-md-9 py-3 text-white">
+              Get connected with us on social networks!
+            </div>
+            <div className="col-md-3 py-3 text-center text-white">
+              <Link to="/" title="Apple">
+                <FontAwesomeIcon icon={faApple} className="text-light mr-3" />
+              </Link>
+              <Link to="/" title="Windows">
+                <FontAwesomeIcon icon={faWindows} className="text-light mr-3" />
+              </Link>
+              <Link to="/" title="Android">
+                <FontAwesomeIcon icon={faAndroid} className="text-light mr-3" />
+              </Link>
+              |
+              <Link to="/" title="Twitter">
+                <FontAwesomeIcon
+                  icon={faTwitter}
+                  className="text-light ml-3 mr-3"
+                />
+              </Link>
+              <Link to="/" title="Facebook">
+                <FontAwesomeIcon
+                  icon={faFacebookF}
+                  className="text-light mr-3"
+                />
+              </Link>
+              <Link to="/" title="Instagram">
+                <FontAwesomeIcon
+                  icon={faInstagram}
+                  className="text-light mr-3"
+                />
+              </Link>
+              <Link to="/" title="Youtube">
+                <FontAwesomeIcon icon={faYoutube} className="text-light mr-3" />
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="container-fluid bg-dark text-white">
+          <div className="row ">
+            <div className="col-md-3 py-3">
+              <div className="h6">Company Name</div>
+              <hr />
+              <p>
+                Our's is the best website to shop online at best deals!
+              </p>
+            </div>
+            <div className="col-md-3 py-3">
+              <div className="h6">Products</div>
+              <hr />
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item bg-dark text-white border-light">
+                  <Link
+                    to="/"
+                    className="text-decoration-none text-white stretched-link"
+                  >
+                    Electronics
+                  </Link>
+                </li>
+                <li className="list-group-item bg-dark text-white border-light">
+                  <Link
+                    to="/"
+                    className="text-decoration-none text-white stretched-link"
+                  >
+                    Mobiles
+                  </Link>
+                </li>
+                <li className="list-group-item bg-dark text-white border-light">
+                  <Link
+                    to="/"
+                    className="text-decoration-none text-white stretched-link"
+                  >
+                    Fashion
+                  </Link>
+                </li>
+                <li className="list-group-item bg-dark text-white border-light">
+                  <Link
+                    to="/"
+                    className="text-decoration-none text-white stretched-link"
+                  >
+                    Jewellery
+                  </Link>
+                </li>
+                
+
+                
+
+                
+                
+              </ul>
+            </div>
+            <div className="col-md-3 py-3">
+              <div className="h6">Policy</div>
+              <hr />
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item bg-dark text-white border-light">
+                  <Link
+                    to="/"
+                    className="text-decoration-none text-white stretched-link"
+                  >
+                    Return Policy
+                  </Link>
+                </li>
+                <li className="list-group-item bg-dark text-white border-light">
+                  <Link
+                    to="/"
+                    className="text-decoration-none text-white stretched-link"
+                  >
+                    Terms Of Use
+                  </Link>
+                </li>
+                <li className="list-group-item bg-dark text-white border-light">
+                  <Link
+                    to="/"
+                    className="text-decoration-none text-white stretched-link"
+                  >
+                    Security
+                  </Link>
+                </li>
+                <li className="list-group-item bg-dark text-white border-light">
+                  <Link
+                    to="/"
+                    className="text-decoration-none text-white stretched-link"
+                  >
+                    Privacy
+                  </Link>
+                </li>
+                <li className="list-group-item bg-dark text-white border-light">
+                  <Link
+                    to="/"
+                    className="text-decoration-none text-white stretched-link"
+                  >
+                    EPR Compliance
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-3 py-3">
+              <div className="h6">Address</div>
+              <hr />
+              <address>
+                <strong>Twitter, Inc.</strong>
+                <br />
+                345 Navi Mumbai 
+                <br />
+                Mumbai, India 94103
+                <br />
+                <abbr title="Phone">P:</abbr> (+91) 9876543210
+              </address>
+              <div className="h6">Customer Care</div>
+              <hr />
+              <IconTelephone /> +91 9876543210
+              <br />
+              <IconEnvelope /> info@email.com
+            </div>
+          </div>
+        </div>
+        <div className="container-fluid bg-secondary text-white text-center">
+          <div className="row">
+            <div className="col-md-2 py-2">
+              <Link to="/" className="text-white text-decoration-none">
+                <IconBriefcase className="text-warning" /> Partner with us
+              </Link>
+            </div>
+            <div className="col-md-2 py-2">
+              <Link to="/" className="text-white text-decoration-none">
+                <IconBadgeAd className="text-info" /> Advertise
+              </Link>
+            </div>
+            <div className="col-md-2 py-2">
+              <Link to="/" className="text-white text-decoration-none">
+                <IconGift className="text-dark" /> Gift
+              </Link>
+            </div>
+            <div className="col-md-3 py-2">
+              © 2021-{new Date().getFullYear()} React-E-Commerce.com
+            </div>
+            
+          </div>
+        </div>
+      </footer>
+    </React.Fragment>
+  );
+};
+export default Footer;
